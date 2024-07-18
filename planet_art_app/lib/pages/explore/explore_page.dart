be9166e75647d600db606dae456b8db1d82956bb
James Han <jayimshan@gmail.com>
2024-07-18 19:59:06 -0400

    Adds default suggestions into gridview

--- planet_art_app/lib/pages/explore/explore_page.dart
+++ planet_art_app/lib/pages/explore/explore_page.dart
@@ -21,12 +21,17 @@ class _ExplorePageState extends State<ExplorePage> {
   var uuid = const Uuid();
   String _sessionToken = '1234567890';
 
+  // Load API KEY from .env
+  String PLACES_API_KEY = dotenv.env['API_KEY'] ?? 'No API key found';
+
   List<dynamic> _places = [];
+  List<Map<String, String>> _photoUrlsWithNames = [];
 
   @override
   void initState() {
     super.initState();
-    _textController.addListener(_onChanged);
+    // _textController.addListener(_onChanged);
+    getSuggestion();
   }
 
   @override
@@ -44,19 +49,16 @@ class _ExplorePageState extends State<ExplorePage> {
           _sessionToken = uuid.v4();
         });
       }
-      getSuggestion(_textController.text);
+      // getSuggestion(_textController.text);
     });
   }
 
-  void getSuggestion(String input) async {
-    // Load API KEY from .env
-    String PLACES_API_KEY = dotenv.env['API_KEY'] ?? 'No API key found';
-
+  void getSuggestion() async {
     try {
       String baseURL =
           'https://maps.googleapis.com/maps/api/place/autocomplete/json';
       String request =
-          '$baseURL?input=$input&key=$PLACES_API_KEY&sessiontoken=$_sessionToken';
+          '$baseURL?input=Museum%20Art&key=$PLACES_API_KEY&sessiontoken=$_sessionToken';
       var response = await http.get(Uri.parse(request));
       var data = json.decode(response.body);
 
@@ -67,8 +69,14 @@ class _ExplorePageState extends State<ExplorePage> {
 
       if (response.statusCode == 200) {
         setState(() {
-          _places = json.decode(response.body)['predictions'];
+          _places = data['predictions'];
         });
+
+        if (_places.isNotEmpty) {
+          var firstPlace = _places.first;
+          await getPlaceDetails(
+              firstPlace['place_id'], firstPlace['description']);
+        }
       } else {
         throw Exception('Failed to load predictions');
       }
@@ -77,23 +85,137 @@ class _ExplorePageState extends State<ExplorePage> {
     }
   }
 
+  Future<void> getPlaceDetails(String placeID, String placeName) async {
+    try {
+      String baseURL =
+          'https://maps.googleapis.com/maps/api/place/details/json';
+      String request = '$baseURL?place_id=$placeID&key=$PLACES_API_KEY';
+      var response = await http.get(Uri.parse(request));
+      var data = json.decode(response.body);
+
+      if (response.statusCode == 200) {
+        var placeDetails = data['result'];
+        var photos = placeDetails['photos'] ?? [];
+
+        _photoUrlsWithNames.clear();
+        for (var photo in photos) {
+          var photoReference = photo['photo_reference'];
+          if (photoReference != null) {
+            var photoUrl = await getPlacePhoto(photoReference);
+            if (photoUrl != null) {
+              _photoUrlsWithNames.add({'url': photoUrl, 'name': placeName});
+            }
+          }
+        }
+
+        setState(() {});
+      } else {
+        throw Exception('Failed to load place details');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<String?> getPlacePhoto(String photoReference) async {
+    try {
+      String baseURL = 'https://maps.googleapis.com/maps/api/place/photo';
+      String request =
+          '$baseURL?maxwidth=400&photoreference=$photoReference&key=$PLACES_API_KEY';
+      var response = await http.get(Uri.parse(request));
+
+      if (response.statusCode == 200) {
+        return response.request?.url.toString();
+      } else {
+        throw Exception('Failed to load photo');
+      }
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text('Explore Page'),
-        ),
-        body: TextField(
-          controller: _textController,
-          decoration: InputDecoration(
-              hintText: 'Search here',
-              suffixIcon: IconButton(
-                icon: const Icon(Icons.cancel_rounded),
-                onPressed: () {
-                  _textController.clear();
-                },
-              )),
-        ));
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Explore Page'),
+      ),
+      body: Column(
+        children: [
+          // TextField(
+          //   controller: _textController,
+          //   decoration: InputDecoration(
+          //     hintText: 'Search here',
+          //     suffixIcon: IconButton(
+          //       icon: const Icon(Icons.cancel_rounded),
+          //       onPressed: () {
+          //         _textController.clear();
+          //       },
+          //     ),
+          //   ),
+          // ),
+          Expanded(
+            child: Stack(
+              children: [
+                ListView.builder(
+                  itemCount: _places.length,
+                  itemBuilder: (context, index) {
+                    var place = _places[index];
+                    return ListTile(
+                      title: Text(place['description']),
+                      // onTap: () async {
+                      //   await getPlaceDetails(
+                      //       place['place_id'], place['description']);
+                      // },
+                    );
+                  },
+                ),
+                if (_photoUrlsWithNames.isNotEmpty)
+                  GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 4.0,
+                      mainAxisSpacing: 4.0,
+                    ),
+                    itemCount: _photoUrlsWithNames.length,
+                    itemBuilder: (context, index) {
+                      var item = _photoUrlsWithNames[index];
+                      return Stack(
+                        children: [
+                          Image.network(
+                            item['url']!,
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                          ),
+                          Positioned(
+                            bottom: 0,
+                            left: 0,
+                            right: 0,
+                            child: Container(
+                              color: Colors.black54,
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                item['name']!,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
