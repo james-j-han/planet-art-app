6a1b94571b421ab68f7cc58a53e868e562bd70f1
unknown <rayadeb101@gmail.com>
2024-07-24 18:05:24 -0400

    updating add_post_page.dart to store uploaded images in firestore by first uploading to storage bucket and retrieving url in database. url located at users/uid/posts. also modifying field sizes and making it scrollable so pixel error stops coming up. tried to fix uri error, but code still functions despite it throwing errors.

--- planet_art_app/lib/pages/account/add_post_page.dart
+++ planet_art_app/lib/pages/account/add_post_page.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../../auth.dart'; 
+import 'user_service.dart'; 
 
 class AddPostPage extends StatefulWidget {
   @override
@@ -9,11 +13,14 @@ class AddPostPage extends StatefulWidget {
 
 class _AddPostPageState extends State<AddPostPage> {
   final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _titleController = TextEditingController();
+
   final TextEditingController _imageUrlController = TextEditingController();
   File? _imageFile;
   String? _imageUrl;
   bool _showUrlField = false;
 
+  //image picker allows file upload from device. not supported on web. 
   Future<void> _pickImage() async {
     final ImagePicker _picker = ImagePicker();
     final XFile? pickedFile = await _picker.pickImage(source: ImageSource.gallery);
@@ -22,42 +29,100 @@ class _AddPostPageState extends State<AddPostPage> {
       setState(() {
         _imageFile = File(pickedFile.path);
         _imageUrl = null;
-        _showUrlField = false; // Hide URL field when picking an image
+        _showUrlField = false;
       });
     }
   }
-
-  void _submitPost() {
+  // post submission
+  Future<void> _submitPost() async {
+    //post fields
     final String description = _descriptionController.text;
+    final String title = _titleController.text;
     final String? imageUrl = _imageUrlController.text.isNotEmpty ? _imageUrlController.text : null;
+    final User? user = FirebaseAuth.instance.currentUser;
 
-    if ((_imageFile != null || imageUrl != null)) {
-      // Here you would typically save the post to a backend or local storage
+    if (user == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('User not logged in')),
+      );
+      return;
+    }
 
+    try {
+      String? postImageUrl;
+      // keeps throwing uri exception, but still works 
       if (_imageFile != null) {
-        print('Image Path: ${_imageFile!.path}');
+        postImageUrl = await _uploadImageToStorage(_imageFile!, user.uid);
+      } else if (imageUrl != null && imageUrl.isNotEmpty) {
+        postImageUrl = _ensureValidUrl(imageUrl);
+      } else {
+        throw Exception('No image selected or URL provided');
       }
-      if (imageUrl != null) {
-        print('Image URL: $imageUrl');
-      }
-      print('Description: $description');
 
-      // Clear the fields after submission
+      if (postImageUrl == null || postImageUrl.isEmpty) {
+        throw Exception('Failed to get a valid image URL');
+      }
+
+      UserService userService = UserService();
+      await userService.addPost(user.uid, postImageUrl, description, title);
+
+      // clear the fields after submission
       setState(() {
         _imageFile = null;
         _imageUrl = null;
         _imageUrlController.clear();
         _descriptionController.clear();
-        _showUrlField = false; // Hide URL field after submission
+        _titleController.clear();
+        _showUrlField = false;
       });
 
-      // Optionally, you can navigate back or show a success message
       Navigator.pop(context);
-    } else {
-      // Show a message if no image or URL is provided
+    } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Please select an image or enter a URL')),
+        SnackBar(content: Text('Error submitting post: ${e.toString()}')),
       );
+    }
+  }
+  // adding to firebase storage. image upload -> url -> stored in firestore db under users/uid/posts
+  Future<String?> _uploadImageToStorage(File imageFile, String userId) async {
+    try {
+      final storageRef = FirebaseStorage.instance
+          .ref()
+          .child('posts/$userId/${DateTime.now().millisecondsSinceEpoch}');
+      
+      final uploadTask = storageRef.putFile(imageFile);
+      
+      uploadTask.snapshotEvents.listen((TaskSnapshot snapshot) {
+        print('Upload progress: ${(snapshot.bytesTransferred / snapshot.totalBytes) * 100}%');
+      });
+      
+      final snapshot = await uploadTask.whenComplete(() {});
+      final downloadUrl = await snapshot.ref.getDownloadURL();
+      print('Image uploaded successfully. URL: $downloadUrl');
+      return downloadUrl;
+    } catch (e) {
+      print('Error uploading image: $e');
+      throw e; // error handled in submit post function
+    }
+  }
+  // handling null input
+  String? _ensureValidUrl(String url) {
+    if (url.isEmpty) {
+      return null;
+    }
+    //trying to fix uri error via parsing and adding http
+    try {
+      Uri parsedUri = Uri.parse(url);
+
+      // check if the URI scheme is missing and add 'http://' if needed
+      if (!parsedUri.hasScheme) {
+        parsedUri = Uri.parse('http://$url');
+      }
+      
+      return parsedUri.toString();
+    } catch (e) {
+      print('Error parsing URL: $e');
+      return null;
     }
   }
 
@@ -65,7 +130,7 @@ class _AddPostPageState extends State<AddPostPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Create new post'),
+        title: Text('New post'),
         actions: [
           TextButton(
             onPressed: _submitPost,
@@ -75,71 +140,88 @@ class _AddPostPageState extends State<AddPostPage> {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Add a Photo',
-              style: TextStyle(fontSize:18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 16),
-            if (_imageFile == null && _imageUrl == null)
-              Text('No image selected.')
-            else
-              Center(
-                child: Container(
-                  width: 100, // Set fixed width
-                  height: 100, // Set fixed height
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey, width: 2),
-                  ),
-                  child: _imageFile != null
-                      ? Image.file(
-                          _imageFile!,
-                          fit: BoxFit.cover,
-                        )
-                      : Image.network(
-                          _imageUrl!,
-                          fit: BoxFit.cover,
-                        ),
-                ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Add a Photo',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
-            SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                ElevatedButton(
-                  onPressed: _pickImage,
-                  child: Text('Choose from gallery'),
+              SizedBox(height: 16),
+              if (_imageFile == null && (_imageUrl == null || _imageUrl!.isEmpty))
+                Text('No image selected.')
+              else
+                Center(
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey, width: 2),
+                    ),
+                    child: _imageFile != null
+                        ? Image.file(
+                            _imageFile!,
+                            fit: BoxFit.cover,
+                          )
+                        : (_imageUrl != null && _imageUrl!.isNotEmpty)
+                            ? Image.network(
+                                _ensureValidUrl(_imageUrl!) ?? '',
+                                fit: BoxFit.cover,
+                                errorBuilder: (context, error, stackTrace) {
+                                  print('Error loading image: $error');
+                                  return Center(child: Text('Error loading image'));
+                                },
+                              )
+                            : Center(child: Text('Invalid image URL')),
+                  ),
                 ),
-                Text('OR'),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      _showUrlField = !_showUrlField;
-                    });
-                  },
-                  child: Text('Add photo URL'),
-                ),
-              ],
-            ),
-            SizedBox(height: 8),
-            if (_showUrlField)
-              _buildTextField(_imageUrlController, 'Image URL'),
-            SizedBox(height: 16),
-            Text(
-              'Add a Caption',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8),
-            _buildTextField(_descriptionController, 'Description', maxLines: 5),
-          ],
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    onPressed: _pickImage,
+                    child: Text('Choose from gallery'),
+                  ),
+                  Text('OR'),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _showUrlField = !_showUrlField;
+                      });
+                    },
+                    child: Text('Add photo URL'),
+                  ),
+                ],
+              ),
+              SizedBox(height: 8),
+              if (_showUrlField)
+                _buildTextField(_imageUrlController, 'Image URL'),
+              SizedBox(height: 16),
+              const Text(
+                'Add a Title',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              _buildTextField(_titleController, 'Title', maxLines: 1),
+              SizedBox(height: 8),
+              const Text(
+                'Add a Caption',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              _buildTextField(_descriptionController, 'Description', maxLines: 2),
+            
+            ],
+          ),
         ),
       ),
     );
   }
+
 
   Widget _buildTextField(TextEditingController controller, String labelText, {int maxLines = 1}) {
     return Padding(
