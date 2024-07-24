7e4c62332f12ab68d2bbd8b5d3a5bd7e0cdab107
unknown <rayadeb101@gmail.com>
2024-07-24 18:22:20 -0400

    handling retrieval and updates

--- planet_art_app/lib/pages/account/user_service.dart
+++ planet_art_app/lib/pages/account/user_service.dart
@@ -15,6 +15,29 @@ class UserService {
     return null;
   }
 
+  Future<List<Map<String, dynamic>>> getUserPosts(String uid) async {
+    try {
+      QuerySnapshot querySnapshot = await _firestore.collection('users').doc(uid).collection('posts').orderBy('timestamp', descending: true).get();
+      return querySnapshot.docs.map((doc) => doc.data() as Map<String, dynamic>).toList();
+    } catch (e) {
+      print("Error fetching user posts: $e");
+      return [];
+    }
+  }
+
+  Future<void> addPost(String uid, String? imageUrl, String description, String title) async {
+    try {
+      await _firestore.collection('users').doc(uid).collection('posts').add({
+        'imageUrl': imageUrl,
+        'description': description,
+        'title': title,
+        'timestamp': FieldValue.serverTimestamp(),
+      });
+    } catch (e) {
+      print("Error adding post: $e");
+    }
+  }
+
   Future<void> saveUserProfile({
     required String name,
     required String occupation,
@@ -32,7 +55,17 @@ class UserService {
         'bio': bio,
         'profileImageUrl': profileImageUrl,
         'pronouns': pronouns,
-        'portfolioLink': portfolioLink, // Ensure correct field name
+        'portfolioLink': portfolioLink, 
+      });
+    }
+  }
+
+  Future<void> updateUserProfileField(String fieldName, String value) async {
+    User? user = _auth.currentUser;
+
+    if (user != null) {
+      await _firestore.collection('users').doc(user.uid).update({
+        fieldName: value,
       });
     }
   }
